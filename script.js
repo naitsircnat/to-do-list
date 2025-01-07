8efe48c3c4f70c9e7e09b053a15655c8e92a6339
naitsircnat <cristiangerardchen@gmail.com>
2025-01-07 02:03:18 +0000

    Enabled modal form pop-up

--- script.js
+++ script.js
@@ -1,47 +1,72 @@
 let createTaskBtn = document.querySelector("#create-task");
 
+
 let taskListing = document.querySelector("#listing");
 
+
 createTaskBtn.addEventListener("click", function () {
-  let taskInput = document.querySelector("#task-input");
+ let taskInput = document.querySelector("#task-input");
 
-  let taskDescript = taskInput.value;
 
-  let taskCard = document.createElement("div");
+ let taskDescript = taskInput.value;
 
-  taskCard.innerHTML = `<div class="card my-2" style="width: 18rem;">
-  <div class="card-body">
-    <p class="card-text">${taskDescript}</p>
-    <a href="#" class="btn btn-primary" id="completed">Completed</a>
-    <a href="#" class="btn btn-secondary" id="edit">Edit</a>
-  </div>
+
+ let taskCard = document.createElement("div");
+
+
+ taskCard.innerHTML = `<div class="card my-2" style="width: 18rem;">
+ <div class="card-body">
+   <p class="card-text">${taskDescript}</p>
+   <a href="#" class="btn btn-primary" id="completed">Completed</a>
+   <a href="#" class="btn btn-secondary" id="edit" data-bs-toggle="modal" data-bs-target="#edit-form">Edit</a>
+ </div>
 </div>`;
 
-  let priInput = document.querySelector("#pri");
 
-  let priority = priInput.value;
+ let priInput = document.querySelector("#pri");
 
-  let impUrgSec = document.querySelector("#imp-urg");
-  let urgSec = document.querySelector("#urg");
-  let impSec = document.querySelector("#imp");
-  let notImpUrgSec = document.querySelector("#not-imp-urg");
 
-  if (priority == "imp-urg") {
-    impUrgSec.appendChild(taskCard);
-  } else if (priority == "urg") {
-    urgSec.appendChild(taskCard);
-  } else if (priority == "imp") {
-    impSec.appendChild(taskCard);
-  } else {
-    notImpUrgSec.appendChild(taskCard);
-  }
+ let priority = priInput.value;
 
-  taskInput.value = "";
+
+ let impUrgSec = document.querySelector("#imp-urg");
+ let urgSec = document.querySelector("#urg");
+ let impSec = document.querySelector("#imp");
+ let notImpUrgSec = document.querySelector("#not-imp-urg");
+
+
+ if (priority == "imp-urg") {
+   impUrgSec.appendChild(taskCard);
+ } else if (priority == "urg") {
+   urgSec.appendChild(taskCard);
+ } else if (priority == "imp") {
+   impSec.appendChild(taskCard);
+ } else {
+   notImpUrgSec.appendChild(taskCard);
+ }
+
+
+ taskInput.value = "";
 });
+
 
 taskListing.addEventListener("click", function (event) {
-  if (event.target.id == "completed") {
-    let card = event.target.closest(".card");
-    card.remove();
-  }
+ if (event.target.id == "completed") {
+   let card = event.target.closest(".card");
+   card.remove();
+ }
 });
+
+
+// taskListing.addEventListener("click", function (event) {
+//   if (event.target.id == "save-changes") {
+//     let card = event.target.closest(".card");
+//     let descript = card.querySelector(".card-text");
+
+
+//     let updatedDescript = document.querySelector("#newDescript").value;
+
+
+//     descript.innerHTML = updatedDescript;
+//   }
+// });
