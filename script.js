c0397e168939d6ed0f49d9333e08f7654ee8046d
naitsircnat <cristiangerardchen@gmail.com>
2025-01-07 03:08:47 +0000

    Added ability to update task description

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 let createTaskBtn = document.querySelector("#create-task");
 
-
 let taskListing = document.querySelector("#listing");
 
-
 createTaskBtn.addEventListener("click", function () {
- let taskInput = document.querySelector("#task-input");
+  let taskInput = document.querySelector("#task-input");
 
+  let taskDescript = taskInput.value;
 
- let taskDescript = taskInput.value;
+  let taskCard = document.createElement("div");
 
-
- let taskCard = document.createElement("div");
-
-
- taskCard.innerHTML = `<div class="card my-2" style="width: 18rem;">
+  taskCard.innerHTML = `<div class="card my-2" style="width: 18rem;">
  <div class="card-body">
    <p class="card-text">${taskDescript}</p>
    <a href="#" class="btn btn-primary" id="completed">Completed</a>
@@ -22,50 +17,70 @@ createTaskBtn.addEventListener("click", function () {
  </div>
 </div>`;
 
+  let priInput = document.querySelector("#pri");
 
- let priInput = document.querySelector("#pri");
+  let priority = priInput.value;
 
+  let impUrgSec = document.querySelector("#imp-urg");
+  let urgSec = document.querySelector("#urg");
+  let impSec = document.querySelector("#imp");
+  let notImpUrgSec = document.querySelector("#not-imp-urg");
 
- let priority = priInput.value;
+  if (priority == "imp-urg") {
+    impUrgSec.appendChild(taskCard);
+  } else if (priority == "urg") {
+    urgSec.appendChild(taskCard);
+  } else if (priority == "imp") {
+    impSec.appendChild(taskCard);
+  } else {
+    notImpUrgSec.appendChild(taskCard);
+  }
 
-
- let impUrgSec = document.querySelector("#imp-urg");
- let urgSec = document.querySelector("#urg");
- let impSec = document.querySelector("#imp");
- let notImpUrgSec = document.querySelector("#not-imp-urg");
-
-
- if (priority == "imp-urg") {
-   impUrgSec.appendChild(taskCard);
- } else if (priority == "urg") {
-   urgSec.appendChild(taskCard);
- } else if (priority == "imp") {
-   impSec.appendChild(taskCard);
- } else {
-   notImpUrgSec.appendChild(taskCard);
- }
-
-
- taskInput.value = "";
+  taskInput.value = "";
 });
 
+let currCard = null;
 
 taskListing.addEventListener("click", function (event) {
- if (event.target.id == "completed") {
-   let card = event.target.closest(".card");
-   card.remove();
- }
+  if (event.target.id == "completed") {
+    currCard = event.target.closest(".card");
+    currCard.remove();
+  }
 });
 
+taskListing.addEventListener("click", function (event) {
+  if (event.target.id == "edit") {
+    currCard = event.target.closest(".card");
+    let currDescript = currCard.querySelector(".card-text").textContent;
+
+    let modalDescriptField = document.querySelector("#newDescript");
+
+    modalDescriptField.value = currDescript;
+  }
+});
+
+saveChangesButton = document.querySelector("#save-changes");
+
+saveChangesButton.addEventListener("click", function () {
+  let modalDescriptField = document.querySelector("#newDescript");
+
+  currCard.querySelector(".card-text").textContent = modalDescriptField.value;
+});
+
+/*
+PULLING CARD DATA INTO FORM
+- After clicking edit, pull description from current card into description field in the form  
+
+UPDATING CARD DATA WITH FORM
+- After clicking "save changes", pull description from form into the card
+*/
 
 // taskListing.addEventListener("click", function (event) {
 //   if (event.target.id == "save-changes") {
 //     let card = event.target.closest(".card");
 //     let descript = card.querySelector(".card-text");
 
-
 //     let updatedDescript = document.querySelector("#newDescript").value;
-
 
 //     descript.innerHTML = updatedDescript;
 //   }
